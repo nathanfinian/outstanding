092059fc4f096776f2f3dc1025e0decd00d97f37
Nathanael Finian <nathanfinian@gmail.com>
2023-10-29 20:39:42 +0700

    Added Datatables Offline, Changed Index

--- index.php
+++ index.php
@@ -15,11 +15,15 @@ require 'cek.php';
         <title>Outstanding</title>
         <link href="css/styles.css" rel="stylesheet" />
         <link rel = "icon" href = "assets/img/mca.png" type = "image/x-icon">
-        <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+
+        <link href="DataTables/datatables.min.css" rel="stylesheet">
+ 
+        <script src="DataTables/datatables.min.js"></script>
+        <!-- <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script type="text/javascript" src="js/calc.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js" crossorigin="anonymous"></script> -->
     </head>
 <body class="sb-nav-fixed">
     <?php
